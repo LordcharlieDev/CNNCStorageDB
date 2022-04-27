583552b180eb01979601ce4dc6377b4564dd454f
Oleksandr <sasha2003demchuk@gmail.com>
2022-04-27 13:40:11 +0300

    Create some classes: Client, Department, Position, Project, Worker.

---
+++ CNNCStorageDB/Models/Project.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CNNCStorageDB.Models
+{
+    public class Project
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Budget { get; set; }
+
+        public decimal FinalPrice { get; set; }
+
+    }
+}
---
+++ CNNCStorageDB/Models/Worker.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CNNCStorageDB.Models
+{
+    public class Worker
+    {
+        public int Id { get; set; }
+        public string FirsName { get; set; }
+        public string MiddleName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public DateTime Birthdate { get; set; }
+        public decimal Salary { get; set; }
+        public decimal Premium { get; set; }
+
+    }
+}
