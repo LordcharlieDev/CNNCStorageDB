fe261a890801f517a71ac69b86ccb040ca2ba014
Oleksandr <sasha2003demchuk@gmail.com>
2022-05-21 18:45:24 +0300

    Add variable CNNCDbContext and constructor.

--- CNNCStorageDB/CNNCDbQueries.cs
+++ CNNCStorageDB/CNNCDbQueries.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CNNCStorageDB.Data;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +9,11 @@ namespace CNNCStorageDB
 {
     public class CNNCDbQueries
     {
+        private readonly CNNCDbContext db;
 
+        public CNNCDbQueries(CNNCDbContext context)
+        {
+            db = context;
+        }
     }
 }
