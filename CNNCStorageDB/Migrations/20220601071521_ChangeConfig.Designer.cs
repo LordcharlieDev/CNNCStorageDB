4139e67370e5b9bbe2d0a59e669fec85c703d373
Oleksandr <sasha2003demchuk@gmail.com>
2022-06-01 10:17:15 +0300

    Add migration ChangeConfig.

--- CNNCStorageDB/Migrations/20220601071521_ChangeConfig.Designer.cs
+++ CNNCStorageDB/Migrations/20220601071521_ChangeConfig.Designer.cs
@@ -4,6 +4,7 @@ using CNNCStorageDB.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace CNNCStorageDB.Migrations
 {
     [DbContext(typeof(CNNCDbContext))]
-    partial class CNNCDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220601071521_ChangeConfig")]
+    partial class ChangeConfig
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ CNNCStorageDB/Migrations/20220601071521_ChangeConfig.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CNNCStorageDB.Migrations
+{
+    public partial class ChangeConfig : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "StartTime",
+                table: "Projects",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(2022, 6, 1, 10, 15, 21, 371, DateTimeKind.Local).AddTicks(9941),
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2",
+                oldDefaultValue: new DateTime(2022, 6, 1, 9, 40, 44, 319, DateTimeKind.Local).AddTicks(4649));
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "EndTime",
+                table: "Projects",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(2022, 6, 1, 11, 15, 21, 372, DateTimeKind.Local).AddTicks(107),
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2",
+                oldDefaultValue: new DateTime(2022, 6, 1, 10, 40, 44, 319, DateTimeKind.Local).AddTicks(4816));
+
+            migrationBuilder.UpdateData(
+                table: "Projects",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "EndTime", "StartTime" },
+                values: new object[] { new DateTime(2022, 6, 1, 10, 15, 21, 372, DateTimeKind.Local).AddTicks(888), new DateTime(2021, 12, 1, 10, 15, 21, 372, DateTimeKind.Local).AddTicks(885) });
+
+            migrationBuilder.UpdateData(
+                table: "Workers",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "Birthdate",
+                value: new DateTime(2022, 6, 1, 10, 15, 21, 372, DateTimeKind.Local).AddTicks(864));
+
+            migrationBuilder.UpdateData(
+                table: "Workers",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "Birthdate",
+                value: new DateTime(2022, 6, 1, 10, 15, 21, 372, DateTimeKind.Local).AddTicks(871));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "StartTime",
+                table: "Projects",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(2022, 6, 1, 9, 40, 44, 319, DateTimeKind.Local).AddTicks(4649),
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2",
+                oldDefaultValue: new DateTime(2022, 6, 1, 10, 15, 21, 371, DateTimeKind.Local).AddTicks(9941));
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "EndTime",
+                table: "Projects",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(2022, 6, 1, 10, 40, 44, 319, DateTimeKind.Local).AddTicks(4816),
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2",
+                oldDefaultValue: new DateTime(2022, 6, 1, 11, 15, 21, 372, DateTimeKind.Local).AddTicks(107));
+
+            migrationBuilder.UpdateData(
+                table: "Projects",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "EndTime", "StartTime" },
+                values: new object[] { new DateTime(2022, 6, 1, 9, 40, 44, 319, DateTimeKind.Local).AddTicks(5696), new DateTime(2021, 12, 1, 9, 40, 44, 319, DateTimeKind.Local).AddTicks(5693) });
+
+            migrationBuilder.UpdateData(
+                table: "Workers",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "Birthdate",
+                value: new DateTime(2022, 6, 1, 9, 40, 44, 319, DateTimeKind.Local).AddTicks(5672));
+
+            migrationBuilder.UpdateData(
+                table: "Workers",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "Birthdate",
+                value: new DateTime(2022, 6, 1, 9, 40, 44, 319, DateTimeKind.Local).AddTicks(5680));
+        }
+    }
+}
