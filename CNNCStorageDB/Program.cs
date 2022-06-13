a9934e68522b0e2bfd28c6032007cc2a8dc5f894
Oleksandr Demchuk <sashademchuk2000@gmail.com>
2022-06-13 11:15:27 +0300

    Change some code.

--- CNNCStorageDB/Program.cs
+++ CNNCStorageDB/Program.cs
@@ -8,9 +8,9 @@ CNNCDbContext db = new CNNCDbContext();
 
 CNNCDbQueries queries = new CNNCDbQueries(db);
 
-//var result = await queries.GetAllWorkers();
+var result = await queries.GetAllWorkers();
 
-//foreach (var item in result)
-//{
-//    Console.WriteLine($"{item.FirstName} {item.MiddleName} {item.LastName} {item.Position.Name} {item.Location.Name}");
-//}
+foreach (var item in result)
+{
+    Console.WriteLine($"{item.FirstName} {item.MiddleName} {item.LastName} {item.Position.Name} {item.Location.Name}");
+}
