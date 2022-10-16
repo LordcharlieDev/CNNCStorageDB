c0a5d26ed5b498a60939b16ebb98cd3a1fa8dcd1
Oleksandr Demchuk <sashademchuk2000@gmail.com>
2022-10-16 15:53:28 +0300

    Add connection string to laptop.

--- CNNCStorageDB/Program.cs
+++ CNNCStorageDB/Program.cs
@@ -1,16 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using CNNCStorageDB;
 using CNNCStorageDB.Data;
+using Microsoft.EntityFrameworkCore;
 
 Console.WriteLine($"{DateTime.Now.AddHours(8)}");
 
 CNNCDbContext db = new CNNCDbContext();
 
-CNNCDbQueries queries = new CNNCDbQueries(db);
 
-var result = await queries.GetAllWorkers();
+var result = await db.Departments.ToListAsync();
 
 foreach (var item in result)
 {
-    Console.WriteLine($"{item.FirstName} {item.MiddleName} {item.LastName} {item.Position.Name} {item.Location.Name}");
+    Console.WriteLine($"{item.Name}");
 }
